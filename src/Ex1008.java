00aa708390bbbc24ab8ba136e9712637573a1527
Jonghun Shin <ozingmw@naver.com>
2020-08-11 19:32:36 +0900

    Ex1008

--- src/Ex1008.java
+++ src/Ex1008.java
@@ -6,7 +6,7 @@ public class Ex1008 {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
-		System.out.println(a/b);
+		System.out.printf("%.10f", (double)a/b);
 	}
 
 }
