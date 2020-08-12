11fea1e7657ea749a15803384b4f8d241b452ca3
Jonghun Shin <69336570+ozingmw@users.noreply.github.com>
2020-08-12 23:23:28 +0900

    Add files via upload

---
+++ src/Ex10172.java
@@ -0,0 +1,11 @@
+
+public class Ex10172 {
+
+	public static void main(String[] args) {
+		System.out.println("|\\_/|\n" + 
+				"|q p|   /}\n" + 
+				"( 0 )\"\"\"\\\n" + 
+				"|\"^\"`    |\n" + 
+				"||_/=\\\\__|");
+	}
+}
